532df871c0fcf54ec44ba725132bdc8b930f4dd6
TempleLo <templelo5408@gmail.com>
2022-04-28 17:48:08 +0800

    Add map activity to select location.

--- app/src/main/java/com/templo/androidcoursefinalproject/UploadItemActivity.java
+++ app/src/main/java/com/templo/androidcoursefinalproject/UploadItemActivity.java
@@ -1,14 +1,46 @@
 package com.templo.androidcoursefinalproject;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class UploadItemActivity extends AppCompatActivity {
+
+    private Button chooseLocationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_item);
+
+        chooseLocationBtn = findViewById(R.id.itemChooseLocBtn);
+        chooseLocationBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MapsSelectLocActivity.class);
+            activityResultLauncher.launch(intent);
+        });
     }
+
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        if (data != null) {
+                        }
+                    }
+                }
+            });
 }
