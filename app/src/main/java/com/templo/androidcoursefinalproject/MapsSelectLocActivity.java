12eafc44b9fbb0ddfbf8558afac90c5bf9edf1cd
TempleLo <templelo5408@gmail.com>
2022-04-28 19:37:41 +0800

    Update map's location to current location using LocationManager.

--- app/src/main/java/com/templo/androidcoursefinalproject/MapsSelectLocActivity.java
+++ app/src/main/java/com/templo/androidcoursefinalproject/MapsSelectLocActivity.java
@@ -1,8 +1,15 @@
 package com.templo.androidcoursefinalproject;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -12,6 +19,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
@@ -25,8 +33,15 @@ import java.util.Arrays;
 
 public class MapsSelectLocActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap mMap = null;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     private ActivityMapsSelectLocBinding binding;
+
+    private Location currentLocation;
+
+    private static final int MAP_MARKER_ZOOM = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +50,78 @@ public class MapsSelectLocActivity extends FragmentActivity implements OnMapRead
         binding = ActivityMapsSelectLocBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        //Set-up location manager for retrieving current location in GPS.
+        setupLocationManager();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        //Set-up auto complete searchbar for location in Google Map.
+        setupPlacesAutoCompleteSupportFragment();
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        if (currentLocation != null) {
+            updateMapMarkerToCurrentLoc();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            }
+        }
+    }
+
+    private void setupLocationManager() {
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) { //When user has changed location.
+                Log.d("LOCATION", location.toString());
+                currentLocation = location;
+                if (mMap != null) {
+                    updateMapMarkerToCurrentLoc();
+                }
+            }
+        };
+
+        //This permission check is required before requestLocationUpdates(). If permission check failed, will request permission explicitly.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            //Request permission explicitly if permission check fails. This will execute onRequestPermissionsResult() callback.
+            //This launches a popup window asking for permission for the LocationManager to use GPS.
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+            //Register for location updates from the given provider with the given arguments, and a callback on the Looper of the calling thread.
+            //2nd arg: minTimeMs – minimum time interval between location updates in milliseconds;
+            //3rd arg: minDistanceM – minimum distance between location updates in meters.
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    private void setupPlacesAutoCompleteSupportFragment() {
         //Places must be initialized so that AutocompleteSupportFragment can be used.
         Places.initialize(getApplicationContext(), getString(R.string.google_api_key));
 
@@ -71,22 +153,10 @@ public class MapsSelectLocActivity extends FragmentActivity implements OnMapRead
         });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void updateMapMarkerToCurrentLoc() {
+        LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        Marker marker = mMap.addMarker(new MarkerOptions().position(currentLatLng).title("Current Location"));
+        marker.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, MAP_MARKER_ZOOM));
     }
 }
